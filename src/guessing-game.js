337a416b825bd8386338506f12df0061d0a22365
VladislavAnkudo <ankudo47@gmail.com>
2023-02-27 21:54:13 +0300

    add file

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.Number = Math.round((this.min + this.max) / 2)
+        return this.Number  
     }
 
     lower() {
-
+        this.max = this.Number
     }
 
     greater() {
-
+        this.min = this.Number
     }
 }
 
